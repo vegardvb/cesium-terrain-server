98eb3e10d03157d8cc0fbee9aa1506a20da5995e
Vegard Vaeng Bernhardsen <vegard-vaeng.bernhardsen@geomatikk.no>
2024-07-16 15:24:25 +0200

    chore: Refactor terrain handler and remove using assets

--- handlers/terrain.go
+++ handlers/terrain.go
@@ -1,13 +1,13 @@
 package handlers
 
 import (
-	"errors"
 	"fmt"
-	"github.com/vegardvb/cesium-terrain-server/assets"
+
+	"net/http"
+
+	"github.com/gorilla/mux"
 	"github.com/vegardvb/cesium-terrain-server/log"
 	"github.com/vegardvb/cesium-terrain-server/stores"
-	"github.com/gorilla/mux"
-	"net/http"
 )
 
 // An HTTP handler which returns a terrain tile resource
@@ -38,27 +38,11 @@ func TerrainHandler(store stores.Storer) func(http.ResponseWriter, *http.Request
 			if store.TilesetStatus(vars["tileset"]) == stores.NOT_FOUND {
 				err = nil
 				http.Error(w,
-					fmt.Errorf("The tileset `%s` does not exist", vars["tileset"]).Error(),
+					fmt.Errorf("the tileset `%s` does not exist", vars["tileset"]).Error(),
 					http.StatusNotFound)
 				return
 			}
 
-			if t.IsRoot() {
-				// serve up a blank tile as it is a missing root tile
-				data, err := assets.Asset("data/smallterrain-blank.terrain")
-				if err != nil {
-					return
-				} else {
-					err = t.UnmarshalBinary(data)
-					if err != nil {
-						return
-					}
-				}
-			} else {
-				err = nil
-				http.Error(w, errors.New("The terrain tile does not exist").Error(), http.StatusNotFound)
-				return
-			}
 		} else if err != nil {
 			return
 		}
